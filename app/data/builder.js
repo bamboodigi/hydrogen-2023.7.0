c4c76e38448a353d2103ef35a7206cac56b4ca17
Blake <blake@bamboodigital.io>
2023-08-09 20:44:23 -0600

    update builder.js

--- app/data/builder.js
+++ app/data/builder.js
@@ -125,21 +125,25 @@ const builderData = {
     "name tape": {
       name: "Name Tape",
       config: {
-        "sizes": [
-          { size: '3” x 1”', maxLength: 9, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 0 },
-          { size: '4” x 1”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: true, sizeUpsell: 1 },
-          { size: '4” x 1.5”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 3 },
-          { size: '5” x 1”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: true, priceUpsell: 2 },
-          { size: '5” x 1.5”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 4 },
-          { size: '6” x 2”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 8 },
-          { size: '8” x 2”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 9 },
-          { size: '8” x 3”', maxLength: 13, lines: 2, placeholder: 'Up to 2 Lines', hasFlag: false, priceUpsell: 14 },
-          { size: '8” x 4”', maxLength: 13, lines: 3, placeholder: 'Up to 3 Lines', hasFlag: false, priceUpsell: 24 },
-          { size: '9” x 3”', maxLength: 13, lines: 2, placeholder: 'Up to 2 Lines of Text', hasFlag: false, priceUpsell: 15, glowInTheDark: 15 },
-          { size: '10” x 2”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: false, priceUpsell: 14, glowInTheDark: 10 },
-          { size: '11” x 3”', maxLength: 16, lines: 2, placeholder: 'Up to 2 Lines', hasFlag: false, priceUpsell: 25 },
-          { size: '12” x 4”', maxLength: 18, lines: 3, placeholder: 'Up to 3 Lines of Text', hasFlag: false, priceUpsell: 41, glowInTheDark: 20 },
-        ]
+        sizes: [
+          { size: '3” x 1”', maxLength: 9, lines: 1, placeholder: 'Your Name', hasFlag: false, sizeUpsell: 0, glowInTheDark: 5 }, 
+          { size: '4” x 1”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: true, sizeUpsell: 1, glowInTheDark: 5 },
+          { size: '4” x 1.5”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: false, sizeUpsell: 3, glowInTheDark: 5 },
+          { size: '5” x 1”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: true, sizeUpsell: 2, glowInTheDark: 5, tacTecCarrier: 2, tacTecTrainer: 3, tRexArms: 0 },
+          { size: '5” x 1.5”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: false, sizeUpsell: 4, glowInTheDark: 5},
+          { size: '6” x 2”', maxLength: 10, lines: 1, placeholder: 'Your Name', hasFlag: false, sizeUpsell: 8, glowInTheDark: 10, policeBlueLine: 5, sheriffBlueLine: 5, },
+          { size: '8” x 2”', maxLength: 10, lines: 1, placeholder: 'Yp to 2 Lines', hasFlag: false, sizeUpsell: 9, glowInTheDark: 10, tRexArms: 4 },
+          { size: '8” x 3”', maxLength: 13, lines: 2, placeholder: 'Up to 2 Lines', hasFlag: false, sizeUpsell: 14, glowInTheDark: 10 },
+          { size: '8” x 4”', maxLength: 13, lines: 3, placeholder: 'Up to 3 Lines', hasFlag: false, sizeUpsell: 24, glowInTheDark: 10 },
+          { size: '9” x 3”', maxLength: 13, lines: 2, placeholder: 'Up to 2 Lines of Text', hasFlag: false, sizeUpsell: 15, glowInTheDark: 15 },
+          { size: '10” x 2”', maxLength: 13, lines: 1, placeholder: 'Your Name', hasFlag: false, sizeUpsell: 14, glowInTheDark: 10 },
+          { size: '11” x 3”', maxLength: 16, lines: 2, placeholder: 'Up to 2 Lines', hasFlag: false, sizeUpsell: 17, glowInTheDark: 15 },
+          { size: '12” x 4”', maxLength: 18, lines: 3, placeholder: 'Up to 3 Lines of Text', hasFlag: false, sizeUpsell: 41, glowInTheDark: 20 },
+        ],
+        extra : [
+            { size: '2.5” x 1”', maxLength: 7, lines: 1, placeholder: 'Your Nm', hasFlag: false, sizeUpsell: 1, glowInTheDark: 5, name : "Custom ID Tag" }, 
+            { size: '5” x 1”', maxLength: 4, lines: 1, placeholder: 'Dad', hasFlag: false, sizeUpsell: 9, glowInTheDark: 7, name : "* As Fuck Name Tape" }, 
+        ],
       },
       form: {
         intro: "Our Name Tape come in various sizes. For sizes 4 x 1, 5 x1 come with the option to add a flag.",
@@ -206,9 +210,9 @@ const builderData = {
       name: "Medical Patch",
       config: {
         "sizes": [
-          { size: '1” x 1”', maxLength: null, lines: null, placeholder: null, hasFlag: false, priceUpsell: 0 },
-          { size: '2” x 2”', maxLength: null, lines: null, placeholder: null, hasFlag: false, priceUpsell: 3 },
-          { size: '3.5” x 2”', maxLength: 12, lines: 1, placeholder: 'nkda', hasFlag: false, priceUpsell: 9 },
+          { size: '1” x 1”', maxLength: null, lines: null, placeholder: null, hasFlag: false, sizeUpsell: 0 },
+          { size: '2” x 2”', maxLength: null, lines: null, placeholder: null, hasFlag: false, sizeUpsell: 3 },
+          { size: '3.5” x 2”', maxLength: 12, lines: 1, placeholder: 'nkda', hasFlag: false, sizeUpsell: 9 },
         ]
       },
       form: {
